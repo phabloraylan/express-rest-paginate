919b98346e098a27c05d5bbef02f2a53300f86ca
phabloraylan <phabloraylan@gmail.com>
2020-03-29 23:34:22 -0300

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = (config = Object) => {
+    return (req, res, next) => {
+        let offset = parseInt(req.query.offset) || (config.offset || 0);
+        let limit = parseInt(req.query.limit) || (config.limit || 10);
+        let sort = req.query.sort || (config.sort || 'createdAt');
+        let desc = req.query.desc == 'true' ? (config.descTrue || 'desc') : (config.descFalse || 'asc');
+
+        if (limit > (config.maxlimit || 100)) {
+            limit = (config.maxlimit || 100);
+        }
+
+        req.paginate = {
+            offset,
+            limit,
+            sort,
+            desc,
+        };
+
+        next();
+    }
+}
