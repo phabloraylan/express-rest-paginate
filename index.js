d83ab5c825197dd0182df9ac4dd8bd00cbc57633
phabloraylan <phabloraylan@gmail.com>
2020-04-07 15:46:41 -0300

    add page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = (config = Object) => {
     return (req, res, next) => {
         let offset = parseInt(req.query.offset) || (config.offset || 0);
         let limit = parseInt(req.query.limit) || (config.limit || 10);
+        let page = limit * offset;
         let sort = req.query.sort || (config.sort || 'createdAt');
         let desc = req.query.desc == 'true' ? (config.descTrue || 'desc') : (config.descFalse || 'asc');
 
@@ -14,6 +15,7 @@ module.exports = (config = Object) => {
             limit,
             sort,
             desc,
+            page
         };
 
         next();
