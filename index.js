f3d26b02c48d2b83de7d106f4e16718531077900
Phablo Raylan <phabloraylan@gmail.com>
2020-04-14 02:54:16 -0300

    limit default to 50

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (config = Object) => {
     return (req, res, next) => {
         let offset = parseInt(req.query.offset) || (config.offset || 0);
-        let limit = parseInt(req.query.limit) || (config.limit || 10);
+        let limit = parseInt(req.query.limit) || (config.limit || 50);
         let page = parseInt(req.query.page) || (config.page || 1);
         let sort = req.query.sort || (config.sort || 'createdAt');
         let desc = req.query.desc == 'true' ? (config.descTrue || 'desc') : (config.descFalse || 'asc');
